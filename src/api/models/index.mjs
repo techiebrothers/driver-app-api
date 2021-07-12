840cc1405f796e6c33a96f610b4d61114173b9c5
Krunal Panchal <krunalpanchal@Krunals-MacBook-Pro.local>
2021-07-12 07:27:03 +0530

    connection string console`d

--- src/api/models/index.mjs
+++ src/api/models/index.mjs
@@ -3,6 +3,7 @@ import { dbConfig } from "../../../globalExports.mjs";
 const ENV = process.env.NODE_ENV || "dev";
 const params = dbConfig[ENV];
 const connectionString = `postgres://${params.username}:${params.password}@${params.host}:${params.port}/${params.database}`
+console.log(connectionString)
 const DB = new SQL(connectionString, {
   define: {
     timestamps: true,
