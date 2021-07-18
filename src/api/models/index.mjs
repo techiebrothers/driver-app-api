3ff21be2e89e1105233da1d1fde5854446cdd6bb
Krunal Panchal <krunalpanchal@Krunals-MacBook-Pro.local>
2021-07-18 15:40:35 +0530

    model changes

--- src/api/models/index.mjs
+++ src/api/models/index.mjs
@@ -1,7 +1,8 @@
+import "dotenv/config"
 import SQL from "sequelize";
 import { dbConfig } from "../../../globalExports.mjs";
 const ENV = process.env.NODE_ENV || "dev";
-// const ENV = 'dev'
+// const ENV = 'test'
 const params = dbConfig[ENV];
 const connectionString = `postgres://${params.username}:${params.password}@${params.host}:${params.port}/${params.database}`
 const DB = new SQL(connectionString, {
